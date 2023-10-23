8dd37e54544d1c8b5ce4fa1f1136eab486dffc65
irsergeev <irsergeev.ewp@gmail.com>
2023-10-24 00:48:08 +0400

    Mappers have been implemented

---
+++ src/ImageManager.Core/Mappers/CommentMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ImageManager.Core.Models;
+using ImageManager.NHibernate.Data;
+
+namespace ImageManager.Core.Mappers
+{
+	public class CommentMapper : Profile
+	{
+		public CommentMapper()
+		{
+			CreateMap<CommentDto, CommentModel>();
+			CreateMap<CommentModel, CommentDto>();
+		}
+	}
+}
---
+++ src/ImageManager.Core/Mappers/ImageMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ImageManager.Core.Models;
+using ImageManager.NHibernate.Data;
+
+namespace ImageManager.Core.Mappers
+{
+	public class ImageMapper : Profile
+	{
+		public ImageMapper()
+		{
+			CreateMap<ImageDto, ImageModel>();
+			CreateMap<ImageModel, ImageDto>();
+		}
+	}
+}
