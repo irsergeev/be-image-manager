e9d8fb3ed83b4963a4b170500bfa47d6fff3dc9d
irsergeev <irsergeev.ewp@gmail.com>
2023-10-23 23:16:40 +0400

    Created base project structure

---
+++ src/ImageManager.Host/Program.cs
@@ -0,0 +1,24 @@
+namespace ImageManager.Host
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			var app = builder.Build();
+
+			app.UseSwagger();
+			app.UseSwaggerUI();
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
