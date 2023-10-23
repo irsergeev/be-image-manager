222d3fc683678e20a8c965368c4dcbf24ca8a61d
irsergeev <irsergeev.ewp@gmail.com>
2023-10-24 01:13:50 +0400

    Project configuration and endpoints have been implemented

--- src/ImageManager.Host/Controllers/ImageController.cs
+++ src/ImageManager.Host/Controllers/ImageController.cs
@@ -1,6 +1,71 @@
-﻿namespace ImageManager.Host.Controllers
+﻿using ImageManager.Core.Interfaces;
+using ImageManager.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageManager.Host.Controllers
 {
+	[ApiController]
+	[Route("[controller]")]
+	[Produces("application/json")]
 	public class ImageController
 	{
+		private readonly IImageService _imageService;
+		
+		public ImageController(IImageService imageService) 
+		{
+			_imageService = imageService;
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<ImageModel>> GetImages()
+		{
+			var result = await _imageService.GetImagesAsync();
+			return result;
+		}
+
+		[HttpPost]
+		public async Task<long> CreateImageAsync([FromBody] ImageModel pictureModel)
+		{
+			var id = await _imageService.CreateImageAsync(pictureModel);
+			return id;
+		}
+
+		[HttpDelete]
+		public async Task DeleteImageAsync([FromQuery] long id)
+		{
+			await _imageService.DeleteImageAsync(id);
+		}
+
+		[HttpPut]
+		public async Task UpdateImageAsync([FromBody] ImageModel picture)
+		{
+			await _imageService.UpdateImageAsync(picture);
+		}
+
+		[HttpGet("{imageId}/Comment")]
+		public async Task<IEnumerable<CommentModel>> GetComments(long imageId)
+		{
+			var comments = await _imageService.GetCommentsAsync(imageId);
+			return comments;
+		}
+
+		[HttpPost("Comment")]
+		public async Task<long> CreateComment([FromBody] CommentModel commentModel)
+		{
+			var commentId = await _imageService.CreateCommentAsync(commentModel);
+			return commentId;
+		}
+
+		[HttpDelete("Comment")]
+		public async Task DeleteCommentAsync([FromQuery] long id)
+		{
+			await _imageService.DeleteCommentAsync(id);
+		}
+
+		[HttpPut("Comment")]
+		public async Task UpdateCommentAsync([FromBody] CommentModel commentModel)
+		{
+			await _imageService.UpdateCommentAsync(commentModel);
+		}
 	}
 }
--- src/ImageManager.Host/Program.cs
+++ src/ImageManager.Host/Program.cs
@@ -1,3 +1,9 @@
+using ImageManager.Core.Interfaces;
+using ImageManager.Core.Mappers;
+using ImageManager.Core.Models.Settings;
+using ImageManager.Core.Services;
+using ImageManager.NHibernate;
+
 namespace ImageManager.Host
 {
 	public class Program
@@ -6,11 +12,30 @@ namespace ImageManager.Host
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-
 			builder.Services.AddControllers();
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+
+			var connectionString = builder.Configuration.GetConnectionString("NHibernate") ?? throw new Exception();
+			builder.Services.AddNHibernate(connectionString);
+			
+			var storageSettings = builder.Configuration
+				.GetRequiredSection(typeof(FileStoreSettings).Name)
+				.Get<FileStoreSettings>() ?? throw new Exception();
+
+			builder.Services.AddSingleton(storageSettings);
+			builder.Services.AddScoped<IFileStoreProvider, FileStoreProvider>();
+
+			builder.Services.AddAutoMapper(new[] { typeof(ImageMapper).Assembly, typeof(CommentMapper).Assembly });
+			builder.Services.AddScoped<IImageService, ImageService>();
+
+			builder.Services.AddCors(options =>
+			{
+				options.AddPolicy("AllowLocalhost", policy => policy
+					.WithOrigins("*")
+					.WithMethods("*")
+					.WithHeaders("*"));
+			});
 
 			var app = builder.Build();
 
